29ac03814a10184a8f1b7a2f44c25f7c2a5b3d86
German Kröger <gkroger11@gmail.com>
2023-07-18 19:10:03 -0300

    much  ass

---
+++ semana 4/script.js
@@ -0,0 +1,15 @@
+function checkAge(age){
+    if (age >= 18){
+        return true;
+    } else{
+        return confirm('Do you have permission from your parents?');
+    }
+}
+
+let age = prompt('How old are you?', 18);
+
+if (checkAge(age)){
+    alert('Access granted');
+} else {
+    alert('Access denied');
+}
